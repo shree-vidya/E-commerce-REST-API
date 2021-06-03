6e0a0d93e006496f7a8586e8db3ad95a910e6e41
shree-vidya <hmshreevidya@gmail.com>
2021-06-03 10:29:55 +0530

    updated packages

--- ap1/routes/user.js
+++ ap1/routes/user.js
@@ -65,7 +65,7 @@ router.post("/login", (req, res, next) => {
                const token = jwt.sign({
                     email: user[0].email,
                     userId: user[0]._id
-                }, process.env.JWT_KEY, 
+                }, "secret", 
                 {
                     expiresIn: "1h"
                 }
